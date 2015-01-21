fbf49bd591d3c2e9aa59259ca4dfb108a0fe5a82
Josep Maria Español Guionnet <josepmeg@190.red-217-124-187.customer.static.ccgg.telefonica.net>
2015-01-21 15:58:47 +0100

    Este es mi primer commit

---
+++ frogLib.js
@@ -0,0 +1,52 @@
+//frogLib.js
+//Objects for frog game
+
+function Fly(){
+	tFly = new Sprite(scene,"pergamino.png",50,50);
+	tFly.setSpeed(10);
+	tFly.wriggle = function (){
+		//change direction by some random amount
+		newDir = (Math.random()*90)-45;
+		this.changeAngleBy(newDir);
+	}//end wriggle
+
+	tFly.reset = function (){
+		//set new random position
+		newX = Math.random() * this.cWidth;
+		newY = Math.random() * this.cHeight;
+		this.setPosition(newX,newY);
+	}//end reset
+
+	tFly.reset();
+
+	return tFly;
+}//end Fly
+
+function Frog(){
+	tFrog = new Sprite (scene, "is.png", 100, 100);
+	tFrog.maxSpeed = 10;
+	tFrog.minSpeed = -3;
+	tFrog.setSpeed(0);
+	tFrog.setAngle(0);
+	tFrog.checkKeys = function(){
+		if (keysDown[K_LEFT]) {
+			this.changeAngleBy(-5);	
+		}//end if
+		if (keysDown[K_RIGHT]) {
+			this.changeAngleBy(5);
+		}//end if
+		if (keysDown[K_UP]) {
+			this.changeSpeedBy(1);
+			if (this.speed > this.maxSpeed) {
+				this.setSpeed(this.maxSpeed);
+			}//end if
+		}//end if
+		if (keysDown[K_DOWN]) {
+			this.changeSpeedBy(-1);
+			if (this.speed < this.minSpeed) {
+				this.setSpeed(this.minSpeed);
+			}//end if
+		}//end if
+	}//end checkKeys
+	return tFrog;
+}//end frog
